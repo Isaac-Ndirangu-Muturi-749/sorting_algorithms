115a559a1a50da75dab4c28634909c4dedf2317c
Isaac Ndirangu Muturi <ndirangumuturi749@gmail.com>
2023-12-19 16:01:08 +0300

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,30 +8,32 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_idx;
+	size_t i, j, min_idx;
 
-    /* Traverse the array */
-    for (i = 0; i < size - 1; i++)
-    {
-        /* Assume the current index is the minimum */
-        min_idx = i;
+	/* Traverse the array */
+	for (i = 0; i < size - 1; i++)
+	{
+		/* Assume the current index is the minimum */
+		min_idx = i;
 
-        /* Find the index of the minimum element in the unsorted part of the array */
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_idx])
-                min_idx = j;
-        }
+		/* Find the index of the minimum element in the unsorted part of the array */
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
 
-        /* Swap the found minimum element with the first element */
-        if (min_idx != i)
-        {
-            int temp = array[i];
-            array[i] = array[min_idx];
-            array[min_idx] = temp;
+		/* Swap the found minimum element with the first element */
+		if (min_idx != i)
+		{
+			int temp;
 
-            /* Print the array after each swap */
-            print_array(array, size);
-        }
-    }
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+
+			/* Print the array after each swap */
+			print_array(array, size);
+		}
+	}
 }
